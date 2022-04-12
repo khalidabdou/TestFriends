9d03cfbb5053dbcec01d3b5dab1509f72cf64fe2
abdellah khalid <37934938+khalidabdou@users.noreply.github.com>
2022-04-12 00:00:44 +0000

    api

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express')
+const port =  5000;
+const app = express();
+const api=require('./routes/api')
+
+app.use('/api',api)
+
+
+
+var server =app.listen(port, () => {
+    console.log(`'server started on port ${port}...'`);
+  });
---
+++ routes/api.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const apiRout = express.Router()
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
+
+
+
+//get user by id 
+apiRout.get('/getUser', async (req, res) => {
+    const user = await prisma.user.findFirst({
+        where: {
+            username: 'ABDELLAH',
+        },
+    })
+    res.send(user)
+})
+
+//create user 
+apiRout.get('/insertUser', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            username: 'ABDELLAH',
+            email: 'elsa@prisma.io',
+            name: 'abdellah khalid',
+            token: 'hgjvbisajhflsakf33455d',
+            image: 'skfskfskaljfik',
+        },
+    })
+    if (user.id) {
+        console.log(user);
+    }
+})
+
+
+//insert result
+apiRout.post('/insertResults', async (req, res) => {
+    let insertRes = await prisma.result.create({
+        data: {
+            sender:10,
+            receiver:11,
+            answers:'{}'
+        }
+    })
+
+    res.send(insertRes)
+})
+
+//get result for user
+apiRout.get('/getResults', async (req, res) => {
+
+})
+
+//get questions 
+apiRout.get('/quetions', (req, res) => {
+
+})
+
+module.exports = apiRout
